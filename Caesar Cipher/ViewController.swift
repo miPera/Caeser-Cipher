7902a6c0c58f5ea7cc3d52923f6a571162a6702f
miPera <migperalta10@gmail.com>
2017-11-18 22:21:25 -0800

    Updated picker view to that holds 3 items

--- Caesar Cipher/ViewController.swift
+++ Caesar Cipher/ViewController.swift
@@ -1,16 +1,17 @@
 import UIKit
 
-class ViewController: UIViewController {
-    var alphabet = [Character]()                //alphabet array
-    @IBOutlet var inputTextField: UITextField!  //Outlet to UITextfield
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet var inputTextField: UITextField!
     @IBOutlet var cipherResult: UILabel!
-    @IBOutlet var picker: UIPickerView!
-
+    @IBOutlet weak var picker: UIPickerView!
+    
+    let pickerData = ["1", "2", "3"]
+    var alphabet = [Character]()            //alphabet array
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         initAlphabet()
-        print(alphabet)
     }
     
     /**
@@ -25,5 +26,33 @@ class ViewController: UIViewController {
     */
     @IBAction func inputTextFieldChanged(_ textField: UITextField) {
         cipherResult.text = textField.text
+    }
+    
+    /**
+        Returns number of columns (components) in picker view
+    */
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    /**
+        Returns title for each row
+    */
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    /**
+        Returns number of rows in picker view
+    */
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    /**
+        Detects element selected in picker view
+    */
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
     }
 }
