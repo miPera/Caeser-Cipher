0b08569f910f5a281a3dbe80aaa184622094b643
miPera <migperalta10@gmail.com>
2017-11-18 18:49:35 -0800

    Added label for cipher result

--- Caesar Cipher/ViewController.swift
+++ Caesar Cipher/ViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class ViewController: UIViewController {
     var alphabet = [Character]()                //alphabet array
     @IBOutlet var inputTextField: UITextField!  //Outlet to UITextfield
+    @IBOutlet var cipherResult: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,11 +12,17 @@ class ViewController: UIViewController {
         print(alphabet)
     }
     
-    
     /**
         Initialize array of characters from the English alphabet
     */
     func initAlphabet () {
         alphabet = (97...122).map({Character(UnicodeScalar($0))})
+    }
+    
+    /**
+        Action function that responds to text being input by user in text field.
+    */
+    @IBAction func inputTextFieldChanged(_ textField: UITextField) {
+        
     }
 }
